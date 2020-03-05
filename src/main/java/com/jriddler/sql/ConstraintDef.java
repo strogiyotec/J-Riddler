1c488e868986e90c2e54853ebe636c3bffb3ec34
strogiyotec <almas337519@gmail.com>
2020-03-04 16:30:43 -0800

    fetch constraints from table

---
+++ src/main/java/com/jriddler/sql/ConstraintDef.java
@@ -0,0 +1,64 @@
+package com.jriddler.sql;
+
+import com.jriddler.contraint.Constraint;
+import com.jriddler.contraint.PgConstraint;
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Get all constraints for specific table.
+ */
+@AllArgsConstructor
+public final class ConstraintDef implements SqlOperation<Map<String, List<Constraint>>> {
+
+    private final String tableName;
+
+    private final JdbcTemplate jdbcTemplate;
+
+    /**
+     * Retrieves all constraints as Map.
+     * Key in map is a column name
+     * Value is a list of constraints for this column
+     *
+     * @return Constraints per column
+     */
+    @Override
+    public Map<String, List<Constraint>> perform() {
+        final List<Map<String, Object>> constraints = this.jdbcTemplate.queryForList(this.query());
+        return constraints
+                .stream()
+                .collect(
+                        Collectors.groupingBy(
+                                map -> map.get("attribute").toString(),
+                                Collectors.mapping(
+                                        PgConstraint::new,
+                                        Collectors.toList()
+                                )
+                        )
+                );
+    }
+
+    private String query() {
+        return String.format(
+                String.join(
+                        "\n",
+                        "SELECT c.conname as name,",
+                        "pg_get_constraintdef(c.oid) as value,",
+                        "a.attname as attribute",
+                        "FROM pg_constraint c",
+                        "INNER JOIN pg_namespace n",
+                        "ON n.oid = c.connamespace",
+                        "CROSS JOIN LATERAL unnest(c.conkey) ak(k)",
+                        "INNER JOIN pg_attribute a",
+                        "ON a.attrelid = c.conrelid",
+                        "AND a.attnum = ak.k",
+                        "WHERE c.conrelid::regclass::text = '%s'",
+                        "ORDER BY c.contype;"
+                ), this.tableName
+        );
+    }
+}
