8e86d204404dcd464eb01d03d019355afe126e48
strogiyotec <almas337519@gmail.com>
2020-03-07 13:57:54 -0800

    change test user name length contraint

--- src/test/resources/db/migration/V2__user_contrains.sql
+++ src/test/resources/db/migration/V2__user_contrains.sql
@@ -2,7 +2,7 @@ ALTER TABLE users
   ADD CONSTRAINT age_positive CHECK (age > 0);
 
 ALTER TABLE users
-    ADD CONSTRAINT name_length CHECK (char_length(name) > 5);
+    ADD CONSTRAINT name_length CHECK (char_length(name) > 1);
 
 ALTER TABLE users
   ADD CONSTRAINT birthday_value CHECK (birthday > '1997-01-01');
