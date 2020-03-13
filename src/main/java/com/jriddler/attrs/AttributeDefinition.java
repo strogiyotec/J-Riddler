b4e9cdbfb0d1a4825749e892c3fd002a973e438c
strogiyotec <almas337519@gmail.com>
2020-03-13 16:39:50 -0700

    ckip autoincremented keys

--- src/main/java/com/jriddler/attrs/AttributeDefinition.java
+++ src/main/java/com/jriddler/attrs/AttributeDefinition.java
@@ -6,6 +6,11 @@ package com.jriddler.attrs;
 public interface AttributeDefinition {
 
     /**
+     * Auto incremented attr.
+     */
+    AttributeDefinition AUTO_INCR = new AutoIncremented();
+
+    /**
      * Value of column.
      *
      * @return Value
@@ -25,5 +30,32 @@ public interface AttributeDefinition {
      * @return Name
      */
     String name();
+
+    /**
+     * Auto generated attr.
+     */
+    final class AutoIncremented implements AttributeDefinition {
+
+        @Override
+        public Object value() {
+            throw new UnsupportedOperationException(
+                    "AutoIncremented attr doesn't have value"
+            );
+        }
+
+        @Override
+        public int size() {
+            throw new UnsupportedOperationException(
+                    "AutoIncremented attr doesn't have size"
+            );
+        }
+
+        @Override
+        public String name() {
+            throw new UnsupportedOperationException(
+                    "AutoIncremented attr doesn't have name"
+            );
+        }
+    }
 
 }
---
+++ src/test/resources/db/migration/V3__managers.sql
@@ -0,0 +1,7 @@
+create table managers
+(
+    id  serial primary key,
+    id2 bigserial,
+    id3 int,
+    id4 bigint
+);
