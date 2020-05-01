b88a3f5fc294233809ffec9a5f1cbb68288c6355
strogiyotec <almas337519@gmail.com>
2020-05-01 12:23:01 -0700

    test case for Version

--- src/main/java/com/jriddler/Version.java
+++ src/main/java/com/jriddler/Version.java
@@ -2,7 +2,7 @@ package com.jriddler;
 
 public final class Version {
 
-    private static final String VERSION = "0.01";
+    public static final String VERSION = "0.01";
 
     String asString() {
         return "JRiddler version \"" + VERSION + "\"";
---
+++ src/test/java/com/jriddler/VersionTestCase.java
@@ -0,0 +1,22 @@
+package com.jriddler;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test {@link Version}.
+ */
+public final class VersionTestCase {
+
+    /**
+     * Test that version contains version number.
+     */
+    @Test
+    public void testVersionString() {
+        Assert.assertThat(
+                new Version().asString(),
+                CoreMatchers.containsString(Version.VERSION)
+        );
+    }
+}
