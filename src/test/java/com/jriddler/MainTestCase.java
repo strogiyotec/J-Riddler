ec6ae3805175c3a3efd1faa4e70f846960890418
strogiyotec <almas337519@gmail.com>
2020-05-06 10:30:43 -0700

    fix test

--- src/test/java/com/jriddler/MainTestCase.java
+++ src/test/java/com/jriddler/MainTestCase.java
@@ -68,7 +68,6 @@ public final class MainTestCase extends TestDbInstance {
     @Test
     @SuppressWarnings("LineLength")
     public void testWithCustomValue() throws SQLException {
-        System.out
         Main.main(
                 new String[]{
                         "-table", "users",
